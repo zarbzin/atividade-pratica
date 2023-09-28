dc09006789fc88488cfbce46adae1a04c2ecaf36
Leandro Souza <leandro.costa@ifba.edu.br>
2023-09-28 15:44:36 -0300

    refactory

--- src/main/java/com/example/javafxdemo/Conta.java
+++ src/main/java/com/example/javafxdemo/Conta.java
@@ -1,6 +1,25 @@
 package com.example.javafxdemo;
 
 public class Conta {
-    public String cliente;
-    public Double saldo = 0d;
+    private Cliente cliente = new Cliente();
+    private double saldo = 0d;
+
+    public void sacar(double valor) {
+        if (saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }
+
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 }
+
